16af89a847ba04a4067acbdbb957793ffb631dcc
Adrian <adriambar@gmail.com>
2015-10-15 18:03:10 +0200

    organized users

--- client/allusers/allusers.js
+++ client/allusers/allusers.js
@@ -2,9 +2,6 @@ Template.body.helpers({
     users: function(){
       return Workers.find();
     },
-    /*singleUser: function(){
-      return new Spacebars.SafeString("<p>It's working! " + this.name +"</p>");
-    }*/
 });
 Template.worker.events({
       'click .clickme': function(event, template){
@@ -12,6 +9,6 @@ Template.worker.events({
         console.log(this.name + ' ' + this.lastName + ' was clicked! ID: ' + this._id);
       },
       'click .delete': function(){
-        Users.remove(this._id);
+        Workers.remove(this._id);
       }
 });
